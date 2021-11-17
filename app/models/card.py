03df807cee137e61bbe8019dbe83c2de41353370
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-11-17 10:39:40 -0800

    DB models setup

--- app/models/card.py
+++ app/models/card.py
@@ -1,12 +1,13 @@
 from .db import db
 
+
 class Card(db.Model):
     __tablename__ = 'cards'
 
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    frontContent = db.Column(db.Text), nullable=False)
-    backContent = db.Column(db.Text), nullable=False)
+    frontContent = db.Column(db.Text, nullable=False)
+    backContent = db.Column(db.Text, nullable=False)
     isPublic = db.Column(db.Boolean, nullable=False)
 
 
@@ -14,11 +15,11 @@ class Card(db.Model):
     user = db.relationship("User", back_populates="cards")
     card_images = db.relationship("Card_Image", back_populates="card_images")
 
-    decks = db.relationship(
-        "Deck", 
-        secondary=deck_card, 
-        back_populates="cards"
-    )
+    # decks = db.relationship(
+    #     "Deck", 
+    #     secondary=deck_card, 
+    #     back_populates="cards"
+    # )
 
 
 
