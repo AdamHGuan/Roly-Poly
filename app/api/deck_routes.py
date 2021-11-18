a3228f6aaa991e2561bbbc193c00ed9a2d5fc5fe
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-11-17 23:49:05 -0800

    Update deck_routes.py

--- app/api/deck_routes.py
+++ app/api/deck_routes.py
@@ -9,36 +9,70 @@ from app.forms import new_deck_form
 deck_routes = Blueprint('decks', __name__)
 
 
-@deck_routes.route('/')
-def all_decks():
+@deck_routes.route('/user/<int:userId>/')
+def user_decks():
     decks = Deck.query.all()
+    decks = Deck.query.filter(Deck.userId == userId).all()
     return {'decks': [deck.to_dict() for deck in decks]}
 
 
 
 
-@deck_routes.route('/', methods=['POST'])
+@deck_routes.route('/user/<int:userId>/', methods=['POST'])
 @login_required
 def create_deck():
 
-  form = new_deck_form()
+  form = deck_form()
   form["csrf_token"].data = request.cookies["csrf_token"]
 
   if form.validate_on_submit():
-    new_deck = Deck(
+    deck = Deck(
         userId = request.json["userId"],
         title=form.data['title'],
         isPublic=form.data['isPublic'],
         deckImgUrl=form.data['deckImgUrl'],
         )
 
-    db.session.add(new_deck)
+    db.session.add(deck)
     db.session.commit()
 
-    return new_deck.to_dict()
+    return deck.to_dict()
 
   else:
     return form.errors
+
+    
+
+@deck_routes.route('/<int:deckId>', methods=['PATCH'])
+@login_required
+def create_deck(deckId):
+
+  form = deck_form()
+  form["csrf_token"].data = request.cookies["csrf_token"]
+
+  if form.validate_on_submit():
+    deck = Deck.query.get(deckId)
+    deck.title = form.data['title']
+    deck.isPublic = form.data['isPublic']
+    deck.deckImgUrl = form.data['deckImgUrl']
+
+    db.session.commit()
+
+    return deck.to_dict()
+
+  else:
+    return form.errors
+
+
+
+@deck_routes.route('/<int:deckId>', methods=['DELETE'])
+def delete_Deck(deckId):
+
+    deck = Deck.query.get(deckId)
+
+    db.session.delete(deck)
+    db.session.commit()
+    return 'Deck deleted.'
 
 
 
