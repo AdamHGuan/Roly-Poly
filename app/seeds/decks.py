830aa84f45d5642dd85b078ca1eaacc25a5afcfd
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-11-18 01:17:07 -0800

    Updated Seeder files

--- app/seeds/decks.py
+++ app/seeds/decks.py
@@ -1,13 +1,14 @@
 from app.models import db, Deck
 
 
-# Adds a demo user, you can add other users here if you want
 def seed_decks():
     deck_1 = Deck(
-        userId='1', title='JavaScript', isPublic=True, deckImgUrl='https://www.javascripttutorial.net/wp-content/uploads/2021/04/JavaScript-Tutorial.svg')
+        userId='1', title='Tarot', isPublic=True, deckImgUrl='https://www.astrology.com/images-US/icons/icon-tarot-daily.svg')
     deck_2 = Deck(
-        userId='1', title='Python', isPublic=True, deckImgUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png')
+        userId='1', title='JavaScript', isPublic=True, deckImgUrl='https://www.javascripttutorial.net/wp-content/uploads/2021/04/JavaScript-Tutorial.svg')
     deck_3 = Deck(
+        userId='1', title='Python', isPublic=True, deckImgUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png')
+    deck_4 = Deck(
         userId='1', title='Test', isPublic=True, deckImgUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png')
 
 
@@ -15,15 +16,13 @@ def seed_decks():
     db.session.add(deck_1)
     db.session.add(deck_2)
     db.session.add(deck_3)
+    db.session.add(deck_4)
+
 
     db.session.commit()
 
 
-# Uses a raw SQL query to TRUNCATE the users table.
-# SQLAlchemy doesn't have a built in function to do this
-# TRUNCATE Removes all the data from the table, and RESET IDENTITY
-# resets the auto incrementing primary key, CASCADE deletes any
-# dependent entities
+
 def undo_decks():
     db.session.execute('TRUNCATE decks RESTART IDENTITY CASCADE;')
     db.session.commit()
