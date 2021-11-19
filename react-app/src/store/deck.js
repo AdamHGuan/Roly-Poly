9885c3463a77b60d2fa8286e8c430c61a1a96f2c
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-11-18 16:44:28 -0800

    Created Deck Redux

---
+++ react-app/src/store/deck.js
@@ -0,0 +1,84 @@
+const GET_DECKS = "deck/GET_DECKS";
+const CREATE_DECK = "deck/CREATE_DECK";
+const DELETE_DECK = "deck/DELETE_DECK";
+
+const getDecks = (data) => ({
+	type: GET_DECKS,
+	data,
+});
+
+const createDeck = (data) => ({
+	type: CREATE_DECK,
+	data,
+});
+
+const deleteDeck = (data) => ({
+	type: DELETE_DECK,
+	data,
+});
+
+export const loadDecks = () => async (dispatch) => {
+	const response = await fetch(`/api/decks/`);
+
+	if (response.ok) {
+		const data = await response.json();
+		dispatch(getDecks(data));
+	}
+};
+
+export const addDeck = (deck) => async (dispatch) => {
+	const response = await fetch(`/api/decks/`, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(deck),
+	});
+
+	if (response.ok) {
+		const data = await response.json();
+		dispatch(createDeck(data));
+		return data;
+	}
+};
+
+export const editDeck = (deck) => async (dispatch) => {
+	const response = await fetch(`/api/decks/${deck.id}`, {
+		method: "PATCH",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(deck),
+	});
+
+	if (response.ok) {
+		const data = await response.json();
+		dispatch(createDeck(data));
+		return data;
+	}
+};
+
+export const deleteSpot = (deck) => async (dispatch) => {
+	const response = await fetch(`/api/decks/${deck.id}`, {
+		method: "DELETE",
+	});
+
+	dispatch(deleteDeck(deck.id));
+};
+
+const deckReducer = (state = {}, action) => {
+	let newState = { ...state };
+
+	switch (action.type) {
+		case LOAD:
+			newState = { ...action.data };
+			return newState;
+		case CREATE:
+			return (newState[action.data.id] = action.data);
+		case UPDATE:
+			return (newState[action.data.id] = action.data);
+		case DELETE:
+			delete newState[action.data.id];
+			return newState;
+		default:
+			return state;
+	}
+};
+
+export default deckReducer;
