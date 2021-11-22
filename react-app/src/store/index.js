1d00335e51323b91fc32a2e1890056e4f2ef4890
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-11-21 20:01:22 -0800

    Added Cards MVP Redux

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import session from "./session";
 import deck from "./deck";
+import card from "./card";
 
 const rootReducer = combineReducers({
 	session,
 	deck,
+	card,
 });
 
 let enhancer;
