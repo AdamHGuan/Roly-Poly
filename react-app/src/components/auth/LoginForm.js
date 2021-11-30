1dfe508ee3aa16055f38cdbe244293c618c142c7
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-11-29 23:17:30 -0800

    Added CSS for Login Form

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { Redirect, useHistory } from "react-router-dom";
 import { login } from "../../store/session";
 
+import "./LoginForm.css";
+
 const LoginForm = () => {
 	const [errors, setErrors] = useState([]);
 	const [email, setEmail] = useState("");
@@ -36,36 +38,60 @@ const LoginForm = () => {
 	}
 
 	return (
-		<form onSubmit={onLogin}>
-			<div>
-				{errors.map((error, ind) => (
-					<div key={ind}>{error}</div>
-				))}
-			</div>
-			<div>
-				<label htmlFor="email">Email</label>
-				<input
-					name="email"
-					type="text"
-					placeholder="Email"
-					value={email}
-					onChange={updateEmail}
-				/>
-			</div>
-			<div>
-				<label htmlFor="password">Password</label>
-				<input
-					name="password"
-					type="password"
-					placeholder="Password"
-					value={password}
-					onChange={updatePassword}
-				/>
-				<button type="submit">Login</button>
-				<button onClick={() => history.push("/sign-up")}>Sign Up</button>
-				<button onClick={handleDemoLogin}>Demo Login</button>
-			</div>
-		</form>
+		<div className="loginForm-main">
+			<form className="loginForm" onSubmit={onLogin}>
+				<h1>Welcome to Roly-Poly!</h1>
+
+				<div className="error-msg-container">
+					{errors.map((error, ind) => (
+						<div className="error-msg" key={ind}>
+							{error}
+						</div>
+					))}
+				</div>
+				<div className="loginForm-section">
+					<label className="loginForm-label" htmlFor="email">
+						Email
+					</label>
+					<input
+						name="email"
+						type="text"
+						placeholder="Email"
+						value={email}
+						onChange={updateEmail}
+					/>
+				</div>
+				<div className="loginForm-section">
+					<label className="loginForm-label" htmlFor="password">
+						Password
+					</label>
+					<input
+						name="password"
+						type="password"
+						placeholder="Password"
+						value={password}
+						onChange={updatePassword}
+					/>
+				</div>
+				<div className="loginForm-btn-container">
+					<button className="btn" type="submit">
+						Login
+					</button>
+				</div>
+				<div className="loginForm-btn-container">
+					<p className="loginForm-btn-msg">New User?</p>
+					<button className="btn" onClick={() => history.push("/sign-up")}>
+						Sign Up
+					</button>
+				</div>
+				<div className="loginForm-btn-container">
+					<p className="loginForm-btn-msg">Demo User?</p>
+					<button className="btn" onClick={handleDemoLogin}>
+						Demo Login
+					</button>
+				</div>
+			</form>
+		</div>
 	);
 };
 
